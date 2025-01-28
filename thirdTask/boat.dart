b3a3112911e0ea47eadda7a2accd39c19a3db1e8
path <f0xyandr111@gmail.com>
2025-01-28 11:34:16 +0500

    thirdTask

---
+++ thirdTask/boat.dart
@@ -0,0 +1,7 @@
+import 'vehicle.dart';
+
+class Boat extends Vehicle {
+  num length;
+
+  Boat(super.model, super.type, this.length);
+}
---
+++ thirdTask/car.dart
@@ -0,0 +1,7 @@
+import 'vehicle.dart';
+
+class Car extends Vehicle {
+  int horsePower;
+
+  Car(super.model, super.type, this.horsePower);
+}
